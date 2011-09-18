2f5ce4fe15773187813124efbe6b38843705769a
George Anderson <yun_man_ger_docs@mail.ru>
2011-09-18 15:58:50 +0600

    reader

--- source/TCReader.java
+++ source/TCReader.java
@@ -3,6 +3,8 @@ import java.io.*;
 
 public class TCReader {
 	static Scanner in;
+	static BufferedReader bin;
+	static boolean eof = false;
 
 	static {
 	    in = new Scanner(System.in);
@@ -12,9 +14,49 @@ public class TCReader {
         return in.nextLine();
 	}
 	
+	public static char readChar(){
+	    int r = 0;
+	    try{
+		r = bin.read();
+		if (r == -1){
+		    eof = true;
+		    r = 0;
+		}
+	    }catch(Exception e){
+	    }
+	    return (char)r;
+	}
+	
+	public static String readString(){
+	    char c = readChar();
+	    while (!eof && c != '\"'){
+		c = readChar();
+	    }
+	    c = readChar();
+	    String r = "";
+	    while(!eof && c != '\"'){
+		r = r + c;
+		c = readChar();
+	    }
+	    return r;
+	}
+	
+	public static String readArray(){
+	    char c = readChar();
+	    while (!eof && c != '{'){
+		c = readChar();
+	    }
+	    c = readChar();
+	    String r = "";
+	    while (!eof && c != '}'){
+		r = r + c;
+		c = readChar();
+	    }
+	    return r;
+	}
+	
 	public static int[] readintArray(){
-	    String s = readline().trim();
-	    s = s.substring(1,s.length()-1);
+	    String s = readArray();
 	    String [] sr = s.split(",");
 	    int [] result = new int[sr.length];
 	    for (int i = 0; i < sr.length; i++){
@@ -24,24 +66,21 @@ public class TCReader {
 	}
 
 	public static double[] readdoubleArray(){
-	    String s = readline().trim();
-	    s = s.substring(1,s.length()-1);
+	    String s = readArray();
 	    String [] sr = s.split(",");
 	    double [] result = new double[sr.length];
 	    for (int i = 0; i < sr.length; i++){
-	        result[i] = Double.parseDouble(sr[i]);
+	        result[i] = Double.parseDouble(sr[i].trim());
 	    }
 	    return result;
 	}
 	
 	public static int readint(){
-	    String s = readline().trim();
-        return Integer.parseInt(s);
+	    return in.nextInt();
 	}
 
 	public static double readdouble(){
-	    String s = readline().trim();
-        return Double.parseDouble(s);
+	    return in.nextDouble();
 	}
 
 	public static String fetchString(String s){
@@ -50,18 +89,13 @@ public class TCReader {
         return s.substring(l,r);
 	}
 	
-	public static String readString(){
-	    String s = readline();
-        return fetchString(s);
-	}
 
 	public static String[] readStringArray(){
-        String s = readline().trim();
-	    s = s.substring(1,s.length()-1);
+	    String s = readArray();
 	    String [] sr = s.split(",");
 	    String [] result = new String[sr.length];
 	    for (int i = 0; i < sr.length; i++){
-	        result[i] = fetchString(sr[i]);
+	        result[i] = fetchString(sr[i].trim());
 	    }
 	    return result;
 	}
